2c26c84930b873b6e495b3515ddf7be7018d183f
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-05-13 10:22:35 -0400

    Feat: Config files

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,7 +1,8 @@
-import './App.css'
+import './App.css';
+import Header from './components/layout/Header';
 
 function App() {
-  
+  return <Header />;
 }
 
-export default App
+export default App;
--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { AuthContextProvider } from './providers/auth/LoginContext.jsx';
+import storage from './utils/localStorage.jsx';
+
+const isLogged = storage.get('auth');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthContextProvider initiallyLogged={isLogged}>
+      <App />
+    </AuthContextProvider>
+  </React.StrictMode>
+);

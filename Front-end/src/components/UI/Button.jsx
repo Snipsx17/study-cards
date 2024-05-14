f7dbb47684927040cfd3ea91b9a32856138f49c4
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-05-13 20:15:34 -0400

    Feat: Add button component

--- Front-end/src/components/UI/Button.jsx
+++ Front-end/src/components/UI/Button.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import './UI.css';
 
-const Button = ({ principal, text, content }) => {
+const Button = ({ principal, text, onClick }) => {
   return (
     <>
       <button
@@ -10,10 +10,7 @@ const Button = ({ principal, text, content }) => {
             ? 'border-0 text-purple px-[30px] py-[9.5px] bg-white hover:bg-slate-100'
             : ' text-white bg-transparent border-2 px-11 py-3'
         }  text-xl  border-white m-4 box-border flex-initial hover:bg-white hover:text-purple`}
-        onClick={() => {
-          console.log(content);
-          return content();
-        }}
+        onClick={onClick}
       >
         {text}
       </button>
@@ -24,7 +21,7 @@ const Button = ({ principal, text, content }) => {
 Button.propTypes = {
   principal: PropTypes.bool,
   text: PropTypes.string,
-  content: PropTypes.func,
+  onClick: PropTypes.func,
 };
 
 export default Button;

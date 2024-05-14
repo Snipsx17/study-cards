050b8dd8484e7bcb897178a919cd6ccb51fe8a72
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-05-13 20:16:38 -0400

    Feat: Add navBar component

--- Front-end/src/components/layout/NavBar.jsx
+++ Front-end/src/components/layout/NavBar.jsx
@@ -1,19 +1,29 @@
+import { useState } from 'react';
 import useLogin from '../../providers/auth/UseLogin';
 import Button from '../UI/Button';
+import Overlay from '../UI/Overlay';
 import UserProfile from '../UI/UserProfile';
 
 const NavBar = () => {
+  const [isVisibleOverlay, setIsVisibleOverlay] = useState(false);
   const auth = useLogin();
 
   return (
-    <nav className="hidden content-center md:flex h-24 ">
-      <Button text="Create" principal />
-      {auth.isLogged ? (
-        <UserProfile userName="Uberth" />
-      ) : (
-        <Button text="Login" />
-      )}
-    </nav>
+    <>
+      <nav className="hidden content-center md:flex h-24 ">
+        <Button
+          text="Create"
+          principal
+          onClick={() => setIsVisibleOverlay(true)}
+        />
+        {auth.isLogged ? (
+          <UserProfile userName="Uberth" />
+        ) : (
+          <Button text="Login" />
+        )}
+      </nav>
+      <Overlay visible={isVisibleOverlay} handleClick={setIsVisibleOverlay} />
+    </>
   );
 };
 

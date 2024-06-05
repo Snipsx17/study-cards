5658045f002f971423b570dd37ab22d41f059298
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-06-05 10:06:44 -0400

    Update user schemas names in routes

--- Back-end/src/v1/auth/auth.ts
+++ Back-end/src/v1/auth/auth.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { buildRequestValidator } from '../../middlewares/validator/buildRequestValidator';
 import { RequestValidatorAdapter } from '../../plugins/requestValidator.plugin';
-import { userValidationSchema } from '../../middlewares/validator/Schemas';
+import {
+  registerUserValidationSchema,
+  loginUserLoginValidationSchema,
+} from '../../middlewares/validator/Schemas';
 import { generateSaltRounds } from '../../utils';
 import { DBClient } from '../../db/DBClient';
-import { hashPassword } from '../../plugins/passwordHash.plugin';
+import {
+  comparePassword,
+  hashPassword,
+} from '../../plugins/passwordHash.plugin';
 export const authRouter = Router();
 
 const requestValidator = new RequestValidatorAdapter();
@@ -13,7 +19,7 @@ const dbClient = new DBClient();
 
 authRouter.post(
   '/register',
-  requestValidatorMiddleware(userValidationSchema),
+  requestValidatorMiddleware(registerUserValidationSchema),
   async (req, res, next) => {
     const { username, email, password } = req.body;
 
@@ -45,6 +51,37 @@ authRouter.post(
     } catch (error) {
       next(error);
       return;
+    }
+  }
+);
+
+authRouter.post(
+  '/login',
+  requestValidatorMiddleware(loginUserLoginValidationSchema),
+  async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      // check if the user exists
+      const userExist = await dbClient.findUserByEmail(email);
+      if (!userExist) {
+        res.status(404);
+        throw new Error(`Invalid username or password`);
+      }
+
+      // check the password
+      const isPasswordValid = await comparePassword(
+        password,
+        userExist.password
+      );
+
+      if (!isPasswordValid) {
+        res.status(404);
+        throw new Error(`Invalid username or password`);
+      }
+
+      res.send(`Welcome ${userExist.username}`);
+    } catch (error) {
+      next(error);
     }
   }
 );

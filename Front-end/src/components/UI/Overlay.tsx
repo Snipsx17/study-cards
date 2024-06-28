42d4f603aa905f8073a224a9b7cf01e6ee934907
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-06-28 19:43:28 -0400

    Add overlay content handler

--- Front-end/src/components/UI/Overlay.tsx
+++ Front-end/src/components/UI/Overlay.tsx
@@ -1,15 +1,18 @@
 type Props = {
   visible: boolean;
   handleClick: (isVisible: boolean) => void;
+  children?: React.ReactNode;
 };
 
-const Overlay = ({ visible, handleClick }: Props) => {
+const Overlay = ({ visible, handleClick, children }: Props) => {
   return visible ? (
     <>
       <div
         onClick={() => handleClick(false)}
-        className={`w-screen h-full bg-black/50 absolute top-0 left-0`}
-      ></div>
+        className={`w-screen h-full bg-black/50 absolute top-0 left-0 flex justify-center items-center`}
+      >
+        {children}
+      </div>
     </>
   ) : null;
 };
--- Front-end/src/components/layout/NavBar.tsx
+++ Front-end/src/components/layout/NavBar.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
 import useLogin from '../../providers/auth/UseLogin';
 import Button from '../UI/Button';
-import Overlay from '../UI/Overlay';
 import UserProfile from '../UI/UserProfile';
 
-const NavBar = () => {
-  const [isVisibleOverlay, setIsVisibleOverlay] = useState(false);
+type Props = {
+  overlayHandler: (isVisible: boolean, content: React.ReactNode) => void;
+};
+
+const NavBar = ({ overlayHandler }: Props) => {
   const auth = useLogin();
 
   return (
@@ -14,15 +15,17 @@ const NavBar = () => {
         <Button
           text="Create"
           principal
-          onClick={() => setIsVisibleOverlay(true)}
+          onClick={() => overlayHandler(true, <p>Create card</p>)}
         />
         {auth?.isLogged ? (
           <UserProfile userName="Uberth" />
         ) : (
-          <Button text="Login" onClick={() => {}} />
+          <Button
+            text="Login"
+            onClick={() => overlayHandler(true, <p>Login</p>)}
+          />
         )}
       </nav>
-      <Overlay visible={isVisibleOverlay} handleClick={setIsVisibleOverlay} />
     </>
   );
 };

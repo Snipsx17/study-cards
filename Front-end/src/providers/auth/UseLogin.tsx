cb7900b6eed4166a0c85c682bd9a294ee844c811
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-07-25 21:10:20 -0400

    Fix useLogin initial status

--- Front-end/src/providers/auth/UseLogin.tsx
+++ Front-end/src/providers/auth/UseLogin.tsx
@@ -1,6 +1,4 @@
 import { useContext } from 'react';
 import { AuthContext } from './LoginContext';
 
-const useLogin = () => useContext(AuthContext);
-
-export default useLogin;
+export const useLogin = () => useContext(AuthContext);

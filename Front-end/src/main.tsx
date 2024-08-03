ae480b4abba6f61f6a770b2d5d911984c6cf28aa
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-08-03 17:35:25 -0400

    fix first lecture of isLogged state in local storage

--- Front-end/src/main.tsx
+++ Front-end/src/main.tsx
@@ -6,7 +6,7 @@ import { AuthContextProvider } from './providers/auth/LoginContext.js';
 import storage from './utils/localStorage.js';
 import { OverlayContextProvider } from './providers/overlay/OverlayContext.js';
 
-const isLogged = storage.get('auth');
+const isLogged = !!storage.get('isLogged');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

bc83bbc9761fb4d3aa01db8e14be08669c91dfdb
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-08-19 21:30:17 -0400

    Manage errors on DBClient

--- Back-end/src/db/DBClient.ts
+++ Back-end/src/db/DBClient.ts
@@ -1,6 +1,6 @@
-import { User as userModel } from '../models';
+import { User as userModel, Category } from '../models';
 import { dbConnection } from './dbConnection';
-import { User } from '../types';
+import { NewCategory, User } from '../types';
 
 export class DBClient {
   async connect(databaseUri: string) {
@@ -14,12 +14,29 @@ export class DBClient {
     return user ? user : null;
   }
 
-  async createUser({ username, email, password }: User): Promise<User> {
-    const user = new userModel({
-      username,
-      email,
-      password,
-    });
-    return await user.save();
+  async createUser({
+    username,
+    email,
+    password,
+  }: User): Promise<User | undefined> {
+    try {
+      const user = new userModel({
+        username,
+        email,
+        password,
+      });
+      return await user.save();
+    } catch (error) {
+      throw new Error('Error creating user');
+    }
+  }
+
+  async addCategory({ category, owner }: NewCategory) {
+    const newCategory = new Category({ name: category, owner });
+    try {
+      return await newCategory.save();
+    } catch (error) {
+      throw new Error('Error saving category');
+    }
   }
 }

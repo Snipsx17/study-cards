60e9e8d009a644062bc07380d76a54ccbc8cb279
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-08-29 15:21:57 -0400

    Create providers component

--- Front-end/src/main.tsx
+++ Front-end/src/main.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Providers } from './providers/Providers.js';
 import App from './App';
 import './index.css';
-import { AuthContextProvider } from './providers/auth/LoginContext.js';
-import storage from './utils/localStorage.js';
-import { OverlayContextProvider } from './providers/overlay/OverlayContext.js';
-import { AsideMenuContextProvider } from './providers/asideMenu/AsideMenuContext.js';
-
-const isLogged = !!storage.get('isLogged');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AuthContextProvider initiallyLogged={isLogged}>
-      <OverlayContextProvider>
-        <AsideMenuContextProvider>
-          <App />
-        </AsideMenuContextProvider>
-      </OverlayContextProvider>
-    </AuthContextProvider>
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>
 );

5ae741fe48f24fcc59e456d97dd20a4c47621b75
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-08-30 11:59:15 -0400

    Add template component

---
+++ Front-end/src/components/layout/Template.tsx
@@ -0,0 +1,12 @@
+import { FC, ReactNode } from 'react';
+import Header from './Header';
+import { Main } from './Main';
+
+export const Template: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <Main>{children}</Main>
+    </>
+  );
+};

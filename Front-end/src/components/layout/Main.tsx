d2ff7b0801eeb3b63048371020054ae85a87931a
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-09-03 15:42:14 -0400

    Add break point to main component

--- Front-end/src/components/layout/Main.tsx
+++ Front-end/src/components/layout/Main.tsx
@@ -1,5 +1,9 @@
 import { FC, ReactNode } from 'react';
 
 export const Main: FC<{ children: ReactNode }> = ({ children }) => {
-  return <main className="my-8 mx-16 flex justify-center">{children}</main>;
+  return (
+    <main className="my-8 mx-10 xl:mx-32 flex justify-center z-0">
+      {children}
+    </main>
+  );
 };

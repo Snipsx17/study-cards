c7537135964ba795e2c43ae91f5f9897a1121c1d
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-09-03 15:43:15 -0400

    Add card grid and card component

---
+++ Front-end/src/components/cards/CardsGrid.tsx
@@ -0,0 +1,48 @@
+import { Card } from './Card';
+
+export const CardsGrid = () => {
+  return (
+    <section className="grid md:grid-cols-cardsGrid2 xl:grid-cols-cardsGrid3 w-full gap-10">
+      <Card
+        question="conjugation verb etre au present"
+        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
+      />
+      <Card
+        question="conjugation verb etre au present"
+        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
+      />
+      <Card
+        question="conjugation verb etre au present"
+        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
+      />
+      <Card
+        question="conjugation verb etre au present"
+        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
+      />
+      <Card
+        question="conjugation verb etre au present"
+        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
+      />
+      <Card
+        question="conjugation verb etre au present"
+        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
+      />
+      <Card
+        question="conjugation verb etre au present"
+        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
+      />
+      <Card
+        question="conjugation verb etre au present"
+        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
+      />
+      <Card
+        question="conjugation verb etre au present"
+        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
+      />
+      <Card
+        question="conjugation verb etre au present"
+        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
+      />
+    </section>
+  );
+};
--- Front-end/tailwind.config.js
+++ Front-end/tailwind.config.js
@@ -5,11 +5,16 @@ export default {
     extend: {
       colors: {
         purple: '#874ccc',
+        purpleButton: '#A161F3',
       },
     },
     fontFamily: {
       sans: ['Helvetica', 'georgia'],
       body: ['Helvetica', 'georgia'],
+    },
+    gridTemplateColumns: {
+      cardsGrid2: 'repeat(2, minmax(300px, 1fr))',
+      cardsGrid3: 'repeat(3, minmax(300px, 1fr))',
     },
   },
   plugins: [],

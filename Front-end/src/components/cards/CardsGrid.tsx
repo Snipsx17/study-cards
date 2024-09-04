14b7fde3514da72ea734390d3e2b4d60aadcbeed
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-09-04 14:13:38 -0400

    Manage estate on grid state

--- Front-end/src/components/cards/CardsGrid.tsx
+++ Front-end/src/components/cards/CardsGrid.tsx
@@ -1,48 +1,14 @@
+import { useState } from 'react';
 import { Card } from './Card';
 
 export const CardsGrid = () => {
+  const [showCardId, setShowCardId] = useState<string | null>(null);
+
+  const handleCardClick = (id: string | null) => {
+    setShowCardId(id);
+  };
+
   return (
-    <section className="grid md:grid-cols-cardsGrid2 xl:grid-cols-cardsGrid3 w-full gap-10">
-      <Card
-        question="conjugation verb etre au present"
-        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
-      />
-      <Card
-        question="conjugation verb etre au present"
-        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
-      />
-      <Card
-        question="conjugation verb etre au present"
-        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
-      />
-      <Card
-        question="conjugation verb etre au present"
-        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
-      />
-      <Card
-        question="conjugation verb etre au present"
-        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
-      />
-      <Card
-        question="conjugation verb etre au present"
-        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
-      />
-      <Card
-        question="conjugation verb etre au present"
-        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
-      />
-      <Card
-        question="conjugation verb etre au present"
-        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
-      />
-      <Card
-        question="conjugation verb etre au present"
-        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
-      />
-      <Card
-        question="conjugation verb etre au present"
-        response="je suis, tu est, il est, nous sommes, vous etes, ils sont"
-      />
-    </section>
+    <section className="grid md:grid-cols-cardsGrid2 xl:grid-cols-cardsGrid3 w-full gap-10"></section>
   );
 };

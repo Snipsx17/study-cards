c1db01402212c37c905b9c4dac7fac8c8b43ada7
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-09-07 23:09:46 -0400

    Get user name on login

--- Front-end/src/providers/user/UseUserData.tsx
+++ Front-end/src/providers/user/UseUserData.tsx
@@ -1,4 +1,4 @@
 import { useContext } from 'react';
-import { userContext } from './userContext';
+import { userContext } from './UserContext';
 
 export const useUserData = () => useContext(userContext);

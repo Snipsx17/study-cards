de6f1427d0cb8e2f706cb20b9913079398a63b79
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-09-07 22:09:51 -0400

    Add cardGrid component

--- Front-end/src/components/cards/CardsGrid.tsx
+++ Front-end/src/components/cards/CardsGrid.tsx
@@ -1,14 +1,54 @@
-import { useState } from 'react';
+import { useLogin } from '@/providers/auth/UseLogin';
+import { useEffect, useState } from 'react';
 import { Card } from './Card';
+import { getTokenJwt } from '@/utils/getTokenJwt';
+import { useGetCards } from '@/hooks/useGetCards';
 
 export const CardsGrid = () => {
-  const [showCardId, setShowCardId] = useState<string | null>(null);
+  const [cardFlipped, setCardFlipped] = useState<string>('');
 
-  const handleCardClick = (id: string | null) => {
-    setShowCardId(id);
+  const { isLogged } = useLogin() ?? {};
+  // const { getFetch, isFetching, error, data } = useFetch();
+  const { loading, error, data: cards, fetchCards, setError } = useGetCards();
+
+  const onClickHandler = (id: string) => {
+    setCardFlipped(id);
   };
 
+  const getCards = async () => {
+    try {
+      const refreshToken = await getTokenJwt();
+      await fetchCards(refreshToken);
+    } catch (error) {
+      if (error instanceof Error) setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (isLogged) {
+      getCards();
+    }
+  }, [isLogged]);
+
   return (
-    <section className="grid md:grid-cols-cardsGrid2 xl:grid-cols-cardsGrid3 w-full gap-10"></section>
+    <section className="grid md:grid-cols-cardsGrid2 xl:grid-cols-cardsGrid3 w-full gap-10">
+      <p>{error}</p>
+      <span>{loading && 'loading....'}</span>
+
+      {isLogged ? (
+        cards?.cards?.map(({ response, question, _id }) => (
+          <Card
+            key={_id}
+            id={_id}
+            response={response}
+            question={question}
+            clickHandler={onClickHandler}
+            showIt={cardFlipped === _id}
+          />
+        ))
+      ) : (
+        <p>Create your first card...</p>
+      )}
+    </section>
   );
 };

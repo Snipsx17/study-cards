73df9723de5f8df387b0222cee53033997a9b4c7
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-09-07 22:08:25 -0400

    Refactor useFetch

--- Front-end/src/hooks/useFetch.ts
+++ Front-end/src/hooks/useFetch.ts
@@ -43,7 +43,7 @@ export const useFetch = () => {
         method: method || 'GET',
         credentials: 'include',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
+        body: body && JSON.stringify(body),
       });
 
       if (!response.ok) {

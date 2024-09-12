79753d69f5f44852121f02727a4c5035d091fde2
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-09-11 21:13:09 -0400

    Refactor layout

--- Front-end/src/components/layout/Main.tsx
+++ Front-end/src/components/layout/Main.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from 'react';
 
 export const Main: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <main className="my-8 mx-10 xl:mx-32 flex justify-center z-0">
+    <main className="my-8 mx-10 xl:mx-32 flex flex-col justify-center z-0">
       {children}
     </main>
   );

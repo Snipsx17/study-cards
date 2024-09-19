fa72e72d2ddce2bb3002f1496f44228ebbba6ad7
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-09-19 19:42:29 -0400

    Refactor submit button

--- Front-end/src/components/forms/button/SubmitButton.tsx
+++ Front-end/src/components/forms/button/SubmitButton.tsx
@@ -5,7 +5,7 @@ type Props = {
   children: string;
   type?: string;
   onClick?: () => void;
-  isFetching: boolean;
+  isFetching?: boolean;
 };
 
 export const SubmitButton = ({ children, isFetching }: Props) => {
@@ -13,6 +13,7 @@ export const SubmitButton = ({ children, isFetching }: Props) => {
     <>
       <button
         className={`block w-[200px] h-16 rounded-full uppercase font-bold text-white px-[30px] py-[9.5px] bg-purple  text-xl mt-4 box-border flex-initial  hover:bg-purple/90 m-auto mb-10`}
+        disabled={isFetching}
       >
         {isFetching ? <Loader /> : children}
       </button>

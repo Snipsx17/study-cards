eff5cde6b3ef52a6a6bd6232be5f67b20bd47bcc
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-09-19 19:42:50 -0400

    Add category selector

--- Front-end/src/components/forms/CategorySelector.tsx
+++ Front-end/src/components/forms/CategorySelector.tsx
@@ -1,4 +1,6 @@
+import { useOverlayContext } from '@/providers';
 import { FC } from 'react';
+import { AddCategory } from './AddCategory';
 
 interface Props {
   label: string;
@@ -6,6 +8,7 @@ interface Props {
 }
 
 export const CategorySelector: FC<Props> = ({ categories, label }) => {
+  const { showOverlay = () => {} } = useOverlayContext() ?? {};
   return (
     <div>
       <label
@@ -14,11 +17,20 @@ export const CategorySelector: FC<Props> = ({ categories, label }) => {
       >
         {label}
       </label>
-      <select className="block w-[300px] text-2xl border-[1px] rounded-xl border-[purple]/50 p-3 text-gray-900 ring-[purple] placeholder:text-gray-400 outline-none focus:border-[purple] focus:border-b-2">
-        {categories.map((category) => {
-          return <option value={category}>{category}</option>;
-        })}
-      </select>
+      <div className="flex gap-3">
+        <select className="block w-4/5 text-2xl border-[1px] rounded-xl border-[purple]/50 p-3 text-gray-900 ring-[purple] placeholder:text-gray-400 outline-none focus:border-[purple] focus:border-b-2">
+          {categories.map((category) => {
+            return <option value={category}>{category}</option>;
+          })}
+        </select>
+        <button
+          title="Create new Category"
+          className="w-1/5 bg-purpleButton text-4xl rounded-md text-white"
+          onClick={() => showOverlay(<AddCategory />)}
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 };
